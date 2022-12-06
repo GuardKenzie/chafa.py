479ae86322451c48823f8afd32367ae0c6be72b6
Erica <tolvukisa@gmail.com>
2022-12-06 11:11:44 +0000

    Example using PIL

---
+++ examples/example_PIL.py
@@ -0,0 +1,53 @@
+import sys
+from chafa import *
+from PIL import Image
+from array import array
+import numpy as np
+
+FONT_WIDTH  = 11
+FONT_HEIGHT = 24
+
+# Init canvas config
+config = CanvasConfig()
+
+# Set canvas height, width and cell geometry
+config.height = 40
+config.width  = 40
+
+# Init the symbol map and add to config
+symbols = SymbolMap()
+symbols.add_by_tags(SymbolTags.CHAFA_SYMBOL_TAG_ALL)
+
+config.set_symbol_map(symbols)
+
+# Open image with PIL
+image = Image.open("./snake.jpg")
+
+width  = image.width
+height = image.height
+bands  = len(image.getbands())
+
+# Put image into correct format
+pixels = np.array(image)
+pixels = np.reshape(pixels, pixels.size)
+
+# Make a buffer for chafa
+pixels = array("B", pixels)
+
+# Calculate canvas size
+config.calc_canvas_geometry(width, height, FONT_WIDTH/FONT_HEIGHT)
+
+# Init the canvas
+canvas = Canvas(config)
+
+# Draw to canvas
+canvas.draw_all_pixels(
+    PixelType.CHAFA_PIXEL_RGB8,
+    pixels,
+    height,
+    width,
+    width * bands
+)
+
+# Write picture
+print(canvas.print().decode())
