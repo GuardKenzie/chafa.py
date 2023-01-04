147a63865ddb85e14a57fa1adc610ea6997d97a5
Erica <tolvukisa@gmail.com>
2023-01-04 23:37:46 +0000

    Added ReadOnlySymbolMap and ReadOnlyCanvasCofig to __init__

--- src/chafa/__init__.py
+++ src/chafa/__init__.py
@@ -6,7 +6,10 @@ from .chafa import SymbolTags
 from .chafa import Optimizations
 
 from .chafa import SymbolMap
+from .chafa import ReadOnlySymbolMap
+from .chafa import Canvas
 from .chafa import CanvasConfig
+from .chafa import ReadOnlyCanvasConfig
 from .chafa import TermDb
 from .chafa import TermInfo
 from .chafa import Canvas
