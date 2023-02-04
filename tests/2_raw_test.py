54db4e68993d07d9b0b539cbe76000badab7ffa9
Erica <tolvukisa@gmail.com>
2023-02-04 19:10:15 +0000

    Tests: Made raw_test better - It now also tests negative canvas slicing

--- tests/2_raw_test.py
+++ tests/2_raw_test.py
@@ -1,24 +1,43 @@
 from chafa import *
 
-def test_raw_color():
-    config = CanvasConfig()
+config = CanvasConfig()
 
-    config.height = config.width = 16
-    config.canvas_mode = CanvasMode.CHAFA_CANVAS_MODE_INDEXED_256
+config.width = 36
+config.height = (256 - (16+24))  // 36 + 2
 
-    pixels = [255, 255, 255]
 
-    canvas = Canvas(config)
-    canvas.draw_all_pixels(
-        PixelType.CHAFA_PIXEL_RGB8,
-        pixels,
-        1, 1, 3
-    )
+config.canvas_mode = CanvasMode.CHAFA_CANVAS_MODE_INDEXED_256
 
-    for y in range(config.height):
-        for x in range(config.width):
-            canvas[y,x].raw_fg_color = ((y * config.width) + x)
-            canvas[y,x].char = "▀"
-            canvas[y,x].raw_bg_color = ((config.height-1 - y) * config.width) + (config.width -1- x)
+pixels = [0, 0, 0, 0]
 
-    print(canvas.print().decode())
+canvas = Canvas(config)
+canvas.draw_all_pixels(
+    PixelType.CHAFA_PIXEL_RGBA8_PREMULTIPLIED,
+    pixels,
+    1, 1, 3
+)
+
+
+for row in canvas[:]:
+    for pix in row:
+        pix.char = "▀"
+        pix.remove_foreground()
+
+
+for pix in canvas[0,:16]:
+    pix.raw_fg_color = pix.x
+
+for row in canvas[1:-1, :]:
+    for pix in row:
+        pix.raw_fg_color = pix.x + 16 + ((pix.y-1) * 5 * config.width//5)
+
+for pix in canvas[-1, :24]:
+    pix.raw_fg_color = 255 - (23 - pix.x)
+
+i = 0
+for pix in canvas[-1, -5:-1]:
+    pix.char = str(i)
+    i+=1
+    pix.raw_fg_color = 3
+
+print(canvas.print().decode())
