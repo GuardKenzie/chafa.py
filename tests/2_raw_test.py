0ba4bea25c02a41dff5164ed798ee90deaa64597
Erica <tolvukisa@gmail.com>
2023-02-11 01:10:56 +0000

    Tests: Made raw_test into an actual test

--- tests/2_raw_test.py
+++ tests/2_raw_test.py
@@ -1,43 +1,44 @@
 from chafa import *
 
-config = CanvasConfig()
+def test_raw():
+    config = CanvasConfig()
 
-config.width = 36
-config.height = (256 - (16+24))  // 36 + 2
-
-
-config.canvas_mode = CanvasMode.CHAFA_CANVAS_MODE_INDEXED_256
-
-pixels = [0, 0, 0, 0]
-
-canvas = Canvas(config)
-canvas.draw_all_pixels(
-    PixelType.CHAFA_PIXEL_RGBA8_PREMULTIPLIED,
-    pixels,
-    1, 1, 3
-)
+    config.width = 36
+    config.height = (256 - (16+24))  // 36 + 2
 
 
-for row in canvas[:]:
-    for pix in row:
-        pix.char = "▀"
-        pix.remove_foreground()
+    config.canvas_mode = CanvasMode.CHAFA_CANVAS_MODE_INDEXED_256
+
+    pixels = [0, 0, 0, 0]
+
+    canvas = Canvas(config)
+    canvas.draw_all_pixels(
+        PixelType.CHAFA_PIXEL_RGBA8_PREMULTIPLIED,
+        pixels,
+        1, 1, 3
+    )
 
 
-for pix in canvas[0,:16]:
-    pix.raw_fg_color = pix.x
+    for row in canvas[:]:
+        for pix in row:
+            pix.char = "▀"
+            pix.remove_foreground()
 
-for row in canvas[1:-1, :]:
-    for pix in row:
-        pix.raw_fg_color = pix.x + 16 + ((pix.y-1) * 5 * config.width//5)
 
-for pix in canvas[-1, :24]:
-    pix.raw_fg_color = 255 - (23 - pix.x)
+    for pix in canvas[0,:16]:
+        pix.raw_fg_color = pix.x
 
-i = 0
-for pix in canvas[-1, -5:-1]:
-    pix.char = str(i)
-    i+=1
-    pix.raw_fg_color = 3
+    for row in canvas[1:-1, :]:
+        for pix in row:
+            pix.raw_fg_color = pix.x + 16 + ((pix.y-1) * 5 * config.width//5)
 
-print(canvas.print().decode())
+    for pix in canvas[-1, :24]:
+        pix.raw_fg_color = 255 - (23 - pix.x)
+
+    i = 0
+    for pix in canvas[-1, -5:-1]:
+        pix.char = str(i)
+        i+=1
+        pix.raw_fg_color = 3
+
+    print(canvas.print().decode())
