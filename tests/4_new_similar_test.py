612ea819d1c0028a51a89a62c042222ecccd3c96
Erica <tolvukisa@gmail.com>
2023-05-21 22:15:26 +0000

    Tests: Added a test for Canvas.new_similar

---
+++ tests/4_new_similar_test.py
@@ -0,0 +1,57 @@
+from chafa import *
+from PIL import Image
+import numpy as np
+from pathlib import Path
+
+def test_PIL():
+    # Init canvas config
+    config = CanvasConfig()
+
+    # Set canvas height and width
+    config.height = 30
+    config.width  = 30
+
+    # Open image with PIL
+    image = Image.open(Path(__file__).parent / "snake.jpg")
+
+    width  = image.width
+    height = image.height
+    bands  = len(image.getbands())
+
+    # Put image into correct format
+    pixels = image.tobytes()
+
+    # Init the canvas and copy it
+    canvas = Canvas(config)
+
+    config.width = 50
+
+    canvas2 = canvas.new_similar()
+    #
+
+    # Draw to canvases
+    canvas.draw_all_pixels(
+        PixelType.CHAFA_PIXEL_RGB8,
+        pixels,
+        height,
+        width,
+        width * bands
+    )
+
+    canvas2.draw_all_pixels(
+        PixelType.CHAFA_PIXEL_RGB8,
+        pixels,
+        height,
+        width,
+        width * bands
+    )
+
+    for pixel in canvas2[:,0]:
+        pixel.bg_color = (255, 0, 0)
+        pixel.char     = " "
+
+    # Write picture
+    print("Original canvas")
+    print(canvas.print().decode())
+    print("Copied canvas")
+    print(canvas2.print().decode())
