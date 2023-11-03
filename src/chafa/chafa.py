1ae28bd804cceb8241b1dcca4b5a55235300795f
Erica <tolvukisa@gmail.com>
2023-11-03 14:30:50 +0000

    Changed: Added a timeout for the get_device_attributes function

--- src/chafa/chafa.py
+++ src/chafa/chafa.py
@@ -2,11 +2,10 @@ from __future__ import annotations
 import sys
 import platform
 from io import UnsupportedOperation
-
+import asyncio
 
 if platform.system() == "Linux":
     import termios
-
 
 def get_device_attributes():
     """
@@ -18,9 +17,9 @@ def get_device_attributes():
 
     :rtype: Tuple[int]
     """
-    
+
     if platform.system() != "Linux":
-        return tuple()
+            return tuple()
 
     try:
         stdin_fileno = sys.stdin.fileno()
@@ -30,47 +29,63 @@ def get_device_attributes():
 
     # Set up
     old_term = termios.tcgetattr(stdin_fileno)
-    new_term = termios.tcgetattr(stdin_fileno)
-
-    # disable canonical mode and disable echo input
-    new_lflags  = ~(termios.ICANON | termios.ECHO) 
-    new_term[3] = new_lflags
-
-    # Emit sequence
-    sys.stdout.write("\033[c")
-    sys.stdout.flush()
     
-    termios.tcsetattr(stdin_fileno, termios.TCSANOW, new_term)
+    
+    async def emit_sequence():
+        # Set up new flags
+        new_term = termios.tcgetattr(stdin_fileno)
 
-    # Read the sequence
-    char = sys.stdin.read(1)
-    attributes = []
+        # disable canonical mode and disable echo input
+        new_lflags  = ~(termios.ICANON | termios.ECHO) 
+        new_term[3] = new_lflags
 
-    while char != "c":
-        attributes.append(char)
+        # Emit sequence
+        sys.stdout.write("\033[c")
+        sys.stdout.flush()
+        
+        termios.tcsetattr(stdin_fileno, termios.TCSANOW, new_term)
+
+        # Read the sequence
         char = sys.stdin.read(1)
+        attributes = []
 
+        while char != "c":
+            attributes.append(char)
+            char = sys.stdin.read(1)
+
+        # Split attributes by ; and turn them into integers
+        if len(attributes) >= 4:
+            attributes = "".join(attributes[3:]).split(";")
+
+            # Convert all attributes to ints unless we can't, then we skip
+            out = []
+            for attrib in attributes:
+                try:
+                    out.append(int(attrib))
+                except ValueError:
+                    continue
+
+            out = tuple(out)
+
+        else:
+            out = tuple()
+
+        return out
+
+    async def timeout():
+        try:
+            return await asyncio.wait_for(emit_sequence(), timeout=2)
+        except asyncio.exceptions.TimeoutError:
+            return tuple()
+
+    # Run timout
+    out = asyncio.run(timeout())
+    
     # Set terminal back to normal
     termios.tcsetattr(stdin_fileno, termios.TCSANOW, old_term)
 
-    # Split attributes by ; and turn them into integers
-    if len(attributes) >= 4:
-        attributes = "".join(attributes[3:]).split(";")
-
-        # Convert all attributes to ints unless we can't, then we skip
-        out = []
-        for attrib in attributes:
-            try:
-                out.append(int(attrib))
-            except ValueError:
-                continue
-
-        out = tuple(out)
-
-    else:
-        out = tuple()
-
     return out
+
 
 
 
